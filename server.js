38f29ae101df23ff9780ce446a87b48a99640b54
andrewetobin <andrewetobin@gmail.com>
2018-12-27 15:53:52 -0700

    Adds url and querystring libraries

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
 
 const environment = process.env.NODE_ENV || 'development';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Publications';
+app.locals.title = 'Weather';
 
+// app.get('/', (request, response) => {
+//   response.send('Here is the weather');
+// });
 app.get('/', (request, response) => {
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  console.log(lat);
+  console.log(lon);
   response.send('Here is the weather');
 });
 
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,3 +7,20 @@ const server = require('../server');
 
 
 chai.use(chaiHttp);
+
+describe('/', () => {
+    it("getting response from weather", done => {
+      chai.request(server)
+        .get("/?lat=37.577473&lon=-105.485443")
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          // response.body[0].should.have.property('id');
+          // response.body[0].should.have.property('name');
+          // response.body[0].should.have.property('artist_name');
+          // response.body[0].should.have.property('genre');
+          // response.body[0].should.have.property('song_rating');
+          done()
+        });
+    });
+  });
