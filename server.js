248f2225f76c831b0b189523518cb387a8de545a
andrewetobin <andrewetobin@gmail.com>
2018-12-28 18:57:06 -0700

    Adds cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const bodyParser = require('body-parser');
 const url = require('url');
@@ -8,14 +10,12 @@ const fetch = require('node-fetch');
 
 const environment = process.env.NODE_ENV || 'development';
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Weather';
 
-// app.get('/', (request, response) => {
-//   response.send('Here is the weather');
-// });
 app.get('/', (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
